e2794f22445ceed6720e0d2781a87faa306381d6
Niclas <Niclas.johansson@consid.se>
2025-07-20 10:59:47 +0200

    refactor: Update EvilEmpire script to improve lane management and clean up code structure

--- Scripts/evil_empire.gd
+++ Scripts/evil_empire.gd
@@ -5,25 +5,20 @@ class_name EvilEmpire
 const bomber = preload("res://Scenes/bomber1.tscn")
 const leftSpawnX = -24
 const rightSpawnX = 272
+const lanes = [null,null,null,null,null,null]
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var timer = $Timer as Timer
 	timer.start(4)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_timer_timeout() -> void:
 	var bomber = bomber.instantiate() as Bomber1
-	
 	get_parent().add_child(bomber)
-	pass # Replace with function body.
 
-func decideSpawnPoint():
+func decideSpawnLane():
+	var noLanesAvailable = lanes.all(func(lane): return lane != null)
+	if noLanesAvailable: return
 	
-	pass
